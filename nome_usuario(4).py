dbae94a4884589fd64ce692f73c5b7e15a6e7912
FilipeUhlig <136847439+FilipeUhlig@users.noreply.github.com>
2023-06-24 10:10:38 -0300

    Add files via upload

---
+++ nome_usuario(4).py
@@ -0,0 +1,17 @@
+"""
+Programa: Nome Usuário
+Descrição: Este programa pergunta o nome do usuário e imprime na tela uma saudação ao nome.
+Autor: Filipe
+Data: 04/06/2023
+Versão: 0.0.1
+"""
+#Atribuição de variáveis
+
+#Entrada de dados
+nome = input("Qual seu nome? ")
+
+#Processamento de dados
+
+
+#Saida de dados
+print(f"Oi, {nome}.")
